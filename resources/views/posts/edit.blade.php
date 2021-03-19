a7a6234c5d5b26b0dd5042a11cbc99dcf84085f6
EC2 Default User <ec2-user@ip-172-31-65-240.ec2.internal>
2021-03-19 07:33:08 +0000

    laravel basic~6

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,33 +4,44 @@
 
         <div class="container mt-4">
            <div class="border p-4">
-               <h1 class="h5 mb-4">投稿の新規作成</h1>
-               <form method="POST" action="{{route('posts.store')}}">
+               <h1 class="h5 mb-4">投稿の編集</h1>
+               <form method="POST" action="{{route('posts.update', ['post' => $post])}}">
                    {{csrf_field()}}
+                   {{method_field('PUT')}}
                    
                    <fieldset class="mb4">
                         <div class="form-group">
                             <label for="title">
                               タイトル
                             </label>
-                            <input class="form-control" id="title" name="title" type="text">
+                            <input
+                              class="form-control"
+                              id="title"
+                              name="title"
+                              value="{{$post->title}}"
+                              type="text"
+                              >
                         </div>
                         <div>
                             <label for="body">
                                 本文
                             </label>
-                            <textarea class="form-control" id="body" name="body" rows="4">
-                                
+                            <textarea
+                              class="form-control"
+                              id="body"
+                              name="body"
+                              rows="4">
+                              {{$post->body}}
                             </textarea>
                         </div>
                         
                         <div class="mt-5">
-                            <a class="btn btn-secondary" href="{{route('top')}}">
+                            <a class="btn btn-secondary" href="{{route('posts.show', ['post' => $post])}}">
                                 キャンセル
                             </a>
                        
                             <button class="btn btn-primary" type="submit">
-                               投稿する
+                               更新する
                             </button>
                         </div>
                    </fieldset>
